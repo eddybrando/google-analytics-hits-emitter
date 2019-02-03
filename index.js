95b6486ec1497fb9d78a4393dd9dcce8c0de9ada
Eddybrando Vásquez <eddy@kickz.com>
2019-02-03 15:03:07 +0100

    feat: Add test to catch ga() being undefined

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 'use strict';
 
-module.exports = function hitsEmitter() {
-  return 'Test: hitsEmitter()';
+var warnings = {
+  gaUndefined: 'W001: "ga()" is undefined. No hits will be send to Google Analytics.',
+};
+
+function hitsEmitter(cb) {
+  if (typeof window.ga === 'undefined') {
+    console.warn(warnings.gaUndefined);
+    return null;
+  }
+  return cb;
+}
+
+function emitEvent() {
+  console.log('Run emitEvent');
+}
+
+module.exports = {
+  emitEvent: hitsEmitter(emitEvent),
 };
