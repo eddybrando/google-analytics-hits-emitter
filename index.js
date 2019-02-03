1a256fd58f2b7b91024b727efdcf5a7dc22f9288
Eddybrando Vásquez <eddy@kickz.com>
2019-02-03 15:39:31 +0100

    fix: Export "sendEvent" as a function

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ var warnings = {
   gaUndefined: 'W001: "ga()" is undefined. No hits will be send to Google Analytics.',
 };
 
-function hitsEmitter(cb) {
-  if (typeof window.ga === 'undefined') {
-    console.warn(warnings.gaUndefined);
-    return null;
-  }
-  return cb;
+function logWarn(msg) {
+  console.warn('[Google Analytics Hits Emitter] ' + msg);
 }
 
-function emitEvent() {
-  console.log('Run emitEvent');
+function hitsEmitter(cb) {
+  if (typeof window.ga === 'undefined') {
+    logWarn(warnings.gaUndefined);
+    return null;
+  }
+  return cb();
+}
+
+function sendEvent() {
+  console.log('Run sendEvent');
 }
 
 module.exports = {
-  emitEvent: hitsEmitter(emitEvent),
+  sendEvent: function() { hitsEmitter(sendEvent); },
 };
